02df4e7547d410848f1010d95ed0ac2c53e8a0b3
larisa <larica290501@mail.ru>
2023-11-07 09:55:49 +0700

    changes

---
+++ catalog/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+from .views import *
+
+
+
+
+
+urlpatterns = [
+    path('', ViewRequests.as_view(), name='index'),
+    path('register/', views.registration, name='register'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login_v, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('request/create', CreateRequest.as_view(), name='request_create'),
+
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
